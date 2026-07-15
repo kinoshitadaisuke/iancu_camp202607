ece1e3c7bea4e4249df12e600028409ea8a70b03
Daisuke <mhdf23888@gmail.com>
2026-07-15 17:14:57 +0800

    adding "py02/camp_202607_py02_02_01.py" on 15/Jul/2026

---
+++ py02/camp_202607_py02_02_01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/07/15 16:10:16 (UT+08:00) daisuke>
+#
+
+# main function
+def main ():
+    # data file name
+    file_data = 'hyperleda.data'
+    # opening file for reading
+    with open (file_data, 'r') as fh_in:
+        # reading file line-by-line
+        for line in fh_in:
+            # skipping line if the line starts with '#'
+            if (line[0] == '#'):
+                continue
+            # skipping line if the line starts with 'objname'
+            if (line[:7] == 'objname'):
+                continue
+            # splitting line
+            (objname, objtype, v_str, v_err_str, \
+             modbest_str, modbest_err_str) = line.split (',')
+            # converting string into float
+            v           = float (v_str)
+            v_err       = float (v_err_str)
+            modbest     = float (modbest_str)
+            modbest_err = float (modbest_err_str)
+            # printing values
+            print (f'{objname:32s}  {objtype:4s}  {modbest:8.3f}  {v:15.3f}')
+        
+
+# executing main function
+if (__name__ == '__main__'):
+    main ()
