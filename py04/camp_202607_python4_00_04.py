8b6b089c706f9f23cff2ac4f8d7c6318f9a6d18e
Daisuke <mhdf23888@gmail.com>
2026-07-17 09:36:54 +0800

    adding "camp_202607_python4_00_04.py" on 17/Jul/2026

---
+++ py04/camp_202607_python4_00_04.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/07/16 10:04:51 (UT+08:00) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# main function
+def main ():
+      # ffmpeg command
+    ffmpeg = 'ffmpeg'
+
+    # ffmpeg options
+    options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
+        + f' -i star_planet_1/star_planet_1_%08d.png' \
+        + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 16'
+
+    # output file name
+    file_output = 'star_planet_1.mp4'
+
+    # command to be executed
+    command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+    
+    # execution of command
+    subprocess.run (command_ffmpeg, shell=True)
+      
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
